55f98dad54da71397e957be08450f20c5af50a46
Md-Shihab-Mizi <67514650+Md-Shihab-Mizi@users.noreply.github.com>
2020-09-05 03:55:06 +0600

    assingment 8 create start

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import Posts from '../Posts/Posts';
+
+const Home = () => {
+    const [posts,setPosts] = useState([]);
+  useEffect(()=>{
+      const url ='https://jsonplaceholder.typicode.com/posts';
+   fetch(url)
+   .then(res => res.json())
+   .then(data =>setPosts(data))
+  },[]);
+  return (
+    <div >
+    <h1> Posts : {posts.length}</h1>
+    {
+        posts.map(post => <Posts post={post}></Posts>)
+    }
+</div>
+  );
+};
+
+export default Home;
